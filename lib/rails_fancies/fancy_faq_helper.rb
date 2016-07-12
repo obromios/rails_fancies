2e9476c3f2d3e2eba96336d830aa1f5de69b587c
Chris Drane <obromios@gmail.com>
2016-07-12 10:36:20 +1000

    change heading to div from h3

--- lib/rails_fancies/fancy_faq_helper.rb
+++ lib/rails_fancies/fancy_faq_helper.rb
@@ -20,7 +20,7 @@ module RailsFancies
         if question_list[name.to_sym].present?
           content_tag :div, class: 'faq_answer' do
             content_tag(:a, '', name: "#{name}") +
-              content_tag(:h3, question_list[name.to_sym]+'?', class: 'faq_answer_heading') +
+              content_tag(:div, question_list[name.to_sym]+'?', class: 'faq_answer_heading') +
               content_tag(:p, "#{text}", class: 'faq_answers')
           end
         else
