d747d8eaf9d2ea777272517ea89ebfb075b26e92
obromios <obromios@gmail.com>
2019-08-31 09:26:56 +1000

    nsg

--- script/build_gem.rb
+++ script/build_gem.rb
@@ -1,10 +1,11 @@
 #!/usr/bin/env ruby
 require '/Users/Chris/Sites/rails_fancies/lib/rails_fancies/version.rb'
 include RailsFancies
+msg = "Build v#{RailsFancies::VERSION} of rails_fancies"
 `bundle`
 `git add .`
-`git commit -m "Build v#{RailsFancies::VERSION} of rails_fancies"`
-`git tag v#{RailsFancies::VERSION}`
+`git commit -m nsg`
+`git tag v#{RailsFancies::VERSION} -m msg`
 `git push origin v#{RailsFancies::VERSION}`
 `git push origin master`
 
