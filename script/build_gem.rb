72d166b42dc7f92be1357f6dff9dbac353c7e25d
obromios <obromios@gmail.com>
2019-08-31 07:34:46 +1000

    Build v1.0.10 of rails_fancies

--- script/build_gem.rb
+++ script/build_gem.rb
@@ -1,13 +1,10 @@
 #!/usr/bin/env ruby
 require '/Users/Chris/Sites/rails_fancies/lib/rails_fancies/version.rb'
 include RailsFancies
-# `gem build rails_fancies`
-# `git add .`
-# `git commit -m "Build v#{RailsFancies::VERSION} of rails_fancies"`
 `bundle`
 `git add .`
-`git commit`
+`git commit -m "Build v#{RailsFancies::VERSION} of rails_fancies"`
+`git tag v#{RailsFancies::VERSION}`
+`git push origin v#{RailsFancies::VERSION}`
 `git push origin master`
-# `gem push rails_fancies-#{RailsFancies::VERSION}.gem`
-`git tag v#{RailsFancies::VERSION} -a`
-`git push origin --tags`
+
