8a852749fff49fc1c1b28cc61e2e4100a7985fc2
Chris Drane <obromois@gmail.com>
2019-12-06 17:41:40 +1100

    Working on upgrading to rails 6

--- script/build_gem.rb
+++ script/build_gem.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env ruby
 require '/Users/Chris/Sites/rails_fancies/lib/rails_fancies/version.rb'
 include RailsFancies
-msg = "Build v#{RailsFancies::VERSION} of rails_fancies"
+msg = "Build v#{RailsFancies::VERSION}"
 `gem build rails_fancies`
 `git add .`
-`git commit -m v1.13`
+system "git commit -m '#{msg}'"
 `git push origin master`
 `gem push rails_fancies-#{RailsFancies::VERSION}.gem`
 `git tag v#{RailsFancies::VERSION} -m msg`
