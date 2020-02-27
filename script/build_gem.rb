640a7a98736cc7179701ae80304131d1db51bf8e
Chris Drane <obromois@gmail.com>
2020-02-27 19:33:32 +1100

    Build v1.0.14

--- script/build_gem.rb
+++ script/build_gem.rb
@@ -1,8 +1,6 @@
 #!/usr/bin/env ruby
 require '/Users/Chris/Sites/rails_fancies/lib/rails_fancies/version.rb'
-require './lib/chris_lib/shell_methods.rb'
 include RailsFancies
-include ShellMethods
 msg = "Build v#{RailsFancies::VERSION}"
 puts msg
 `gem build rails_fancies`
@@ -16,3 +14,11 @@ system "gem push rails_fancies-#{RailsFancies::VERSION}.gem"
 puts "pushed v#{RailsFancies::VERSION} tag"
 git_sha = `git log --pretty=format:'%h' -n 1`
 puts "Bump version, update changelog with #{git_sha} #{time_hash}"
+
+BEGIN {
+  def time_hash
+  time = Time.now
+  "#{time.day}-#{time.month}-#{time.year} #{time.hour}:#{time.min}"
+  end
+}
+
